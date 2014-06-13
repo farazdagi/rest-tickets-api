23399c5352a007c7ba09da607f834b6322c3c962
Victor Farazdagi <simple.square@gmail.com>
2014-06-13 21:39:50 +0400

    fos/restbundle is installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
