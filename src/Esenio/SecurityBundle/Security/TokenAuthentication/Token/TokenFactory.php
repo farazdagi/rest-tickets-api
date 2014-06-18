f9834c7acd24e845cc6c31791b97110602fa510f
Victor Farazdagi <simple.square@gmail.com>
2014-06-19 00:43:56 +0400

    Remove UserInterface requirement in TokenFactory

--- src/Esenio/SecurityBundle/Security/TokenAuthentication/Token/TokenFactory.php
+++ src/Esenio/SecurityBundle/Security/TokenAuthentication/Token/TokenFactory.php
@@ -14,13 +14,13 @@ class TokenFactory implements TokenFactoryInterface
     /**
      * Creates TokenInterface object. If UserInterface parameter is not supplied, anonymous token is created.
      *
-     * @param UserInterface $user User associated with token
+     * @param string|UserInterface $user User associated with token
      * @param $credentials String representation of token (encoded).
      * @param array $roles Roles assigned to token, token gets authenticated if roles are not empty
      *
      * @return TokenInterface
      */
-    public function createToken(UserInterface $user = null, $credentials = '', array $roles = array())
+    public function createToken($user = null, $credentials = '', array $roles = array())
     {
         if (!$user) {
             $user = TokenInterface::USER_ANONYMOUS;
